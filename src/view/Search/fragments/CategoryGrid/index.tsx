55fd7b8323ebfd78c401fdf6ef369fbe8478233a
Bárbara Boechat <barbs.boechat@gmail.com>
2024-10-19 17:35:41 -0300

    feature(category-grid): Adição de chaves que estavam faltando nos maps do grid

--- src/view/Search/fragments/CategoryGrid/index.tsx
+++ src/view/Search/fragments/CategoryGrid/index.tsx
@@ -20,28 +20,27 @@ const CategoryGrid: React.FC<CategoryGridProps> = ({ categorizedBooks }) => {
   return (
     <CategorizedBooksContainer>
       {Object.entries(categorizedBooks).map(([category, books]) => (
-        <>
+        <div key={category}>
           <CategoryTitle>{category}</CategoryTitle>
           <ContentResults>
             {books.map(({ id, volumeInfo: book }) => (
-              <>
-                <ContentResultsWrapper
-                  onClick={() => navigate("/book/" + id, { state: book })}
-                >
-                  <ContentResultsCover>
-                    <img src={book.imageLinks?.thumbnail} alt={book.title} />
-                  </ContentResultsCover>
-                  <ContentResultsTitle>
-                    <label>{book.title} </label>
-                  </ContentResultsTitle>
-                  <ContentResultsCategory>
-                    <span>{book.authors?.join(", ")}</span>
-                  </ContentResultsCategory>
-                </ContentResultsWrapper>
-              </>
+              <ContentResultsWrapper
+                key={book.title}
+                onClick={() => navigate("/book/" + id, { state: book })}
+              >
+                <ContentResultsCover>
+                  <img src={book.imageLinks?.thumbnail} alt={book.title} />
+                </ContentResultsCover>
+                <ContentResultsTitle>
+                  <label>{book.title} </label>
+                </ContentResultsTitle>
+                <ContentResultsCategory>
+                  <span>{book.authors?.join(", ")}</span>
+                </ContentResultsCategory>
+              </ContentResultsWrapper>
             ))}
           </ContentResults>
-        </>
+        </div>
       ))}
     </CategorizedBooksContainer>
   );
