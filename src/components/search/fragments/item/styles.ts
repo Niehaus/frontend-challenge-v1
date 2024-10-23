f4b71bff3c01abf33703bb2999135bdd633d2f3a
Bárbara Boechat <barbs.boechat@gmail.com>
2024-10-23 09:38:31 -0300

    feature(overlay-items):Quebra dos itens de resposta em um fragmento de item

---
+++ src/components/search/fragments/item/styles.ts
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+export const Item = styled.li`
+    display: flex;
+    align-items: center;
+
+
+    list-style: none;
+    padding: 16px;
+    cursor: pointer;
+    border-radius: 50px;
+
+    &:hover {
+        background-color: #DAF6F3;
+    }
+`
+
+export const ItemImg = styled.div`
+    max-width: 40px;
+    flex-shrink: 0;
+`
+
+export const ItemContent = styled.div`
+    padding: 8px;
+    display: flex;
+    flex-direction: column;
+
+    flex-grow: 1;
+`
+
+export const Empty = styled.li`
+    list-style: none;
+    padding: 8px 16px;
+`
+
+export const ResultLink = styled(Link)`
+    color: inherit;
+
+    &:hover {
+        color: inherit;
+    }
+`
